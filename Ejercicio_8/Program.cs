df461e4eefa999020393120898925284664bee9a
Gust-CM <jervis344.89@gmail.com>
2025-09-18 20:37:35 -0600

    Agrego los últimos 3 ejercicios pendientes. Listo para entregar al curso.

---
+++ Ejercicio_8/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        int opcion; //Variable para almacenar la opción del usuario
+
+        do
+        {
+            Console.WriteLine("\n--- Menú Interactivo ---"); //Inicio del menú interactivo
+            Console.WriteLine("1. Saludar");
+            Console.WriteLine("2. Fecha de hoy");
+            Console.WriteLine("3. Salir del menú");
+            Console.WriteLine("Seleccione una opción entre 1, 2 o 3");
+
+            opcion = Convert.ToInt32(Console.ReadLine()); //Lee la opción del usuario
+
+            switch (opcion)
+            {
+                case 1:
+                    Console.WriteLine("¡Hola! ¿Cómo estás?");
+                    break;
+                case 2:
+                    Console.WriteLine($"La fecha de hoy es: {DateTime.Now}");
+                    break;
+                case 3:
+                    Console.WriteLine($"Saliendo del menú, digitaste la opción: {opcion}");
+                    break;
+                default:
+                    Console.WriteLine($"Digitaste la opción: {opcion} agrega una opción correcta."); break;
+            }
+        }
+        while (opcion != 3); //El menú se repetirá hasta que el usuario elija la opción 3 para salir del menú
+    }
+}
---
+++ Ejercicio_9/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        double suma = 0;
+        int contador = 0;
+        double numero;
+
+        Console.WriteLine("Ingrese números para calcular el promedio. Escribe -1 para terminar");
+
+        do
+        {
+            Console.WriteLine("Introduce un número: ");
+            numero = Convert.ToDouble(Console.ReadLine());
+
+            if (numero != -1)
+            {
+                suma += numero;
+                contador++;
+            }
+        }
+        while (numero != -1);
+
+        if (contador > 0)
+        {
+            double promedio = suma / contador;
+            Console.WriteLine($"El promedio es: {promedio}");
+        }
+        else
+        {
+            Console.WriteLine("No se ingresaron los datos suficientes. ");
+        }
+    }
+}
